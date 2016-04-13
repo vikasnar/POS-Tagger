5c8446436c7f137f307180d8faff9ab73b920128
VikasN <vikas.n649@gmail.com>
2016-04-12 17:07:45 -0700

    Smaller Test Set

--- hmmdecode.py
+++ hmmdecode.py
@@ -1,81 +1,140 @@
 import codecs
 import sys
-import math
+import time
 
 model = codecs.open('hmmmodel.txt', 'r', 'utf-8')
 out_file = codecs.open('hmmoutput.txt', 'w', 'utf-8')
 transition = {}
 emission = {}
-tag_set = set()
+tag_count = {}
+last_tag = {}
 
 
 def read_model():
     line = model.readline()
     while line:
-        data = line.strip().split(' ')
+        data = line.strip().split()
         if data[0] == 'T':
-            transition[data[1]+" "+data[2]] = float(data[3])
-            tag_set.add(data[1])
+            transition[data[1]] = eval(model.readline())
         elif data[0] == 'E':
-            emission[data[1]+" "+data[2]] = float(data[3])
+            emission[data[1]] = eval(model.readline())
+        elif data[0] == "Count":
+            global tag_count
+            tag_count = eval(model.readline())
+        elif data[0] == "Last Tags":
+            global last_tag
+            last_tag = eval(model.readline())
         line = model.readline()
 
 
 def process_input(path):
     input_file = codecs.open(path, 'r', 'utf-8')
     line = input_file.readline()
-    state_prob = {}
-    best_prob = {}
-    state_prob["0 <s>"] = 0
-    best_prob["0 <s>"] = None
+    tag_set = tag_count.keys()
+    t_len = len(tag_count) - 1
     while line:
-        words = line.strip().split(" ")
-        length = len(words)
-        for i in range(0, length):
-            for prev in tag_set:
-                for nxt in tag_set:
-                    cur_state = "{} {}".format(i, prev)
-                    nxt_state = "{} {}".format(i+1, nxt)
-                    tran_key = "{} {}".format(prev, nxt)
-                    em_key = "{} {}".format(nxt, words[i])
-                    if cur_state in state_prob and tran_key in transition:
-                        if em_key in emission:
-                            prob = state_prob[cur_state] + (-1 * math.log(transition[tran_key])) + (-1 * math.log(emission[em_key]))
-                        else:
-                            prob = state_prob[cur_state] + (-1 * math.log(transition[tran_key]))
-                        if nxt_state not in state_prob or state_prob[nxt_state] > prob:
-                            state_prob[nxt_state] = prob
-                            best_prob[nxt_state] = cur_state
+        words = line.split()
+        viterbi = []
+        backpointer = []
+        first_tag = {}
+        first_backpointer = {}
+        cur_word = words[0]
+        if cur_word in emission:
+            possible_tags = emission[cur_word].keys()
+        else:
+            possible_tags = tag_set
+        for tag in possible_tags:
+            if tag == "<s>":
+                continue
+            first_tag[tag] = transition_probability("<s>", tag, t_len) * emission_probability(cur_word, tag)
+            first_backpointer[tag] = "<s>"
 
-        nxt = " </s>"
-        for prev in tag_set:
-            prev_state = "{} {}".format(i, prev)
-            end_state = "{}{}".format(i+1, nxt)
-            end_key = "{}{}".format(prev, nxt)
-            if prev_state in state_prob and end_key in transition:
-                prob = state_prob[prev_state] + (-1 * math.log(transition[end_key]))
-                if end_state not in state_prob or state_prob[end_state] > prob:
-                    state_prob[end_state] = prob
-                    best_prob[end_state] = prev_state
+        viterbi.append(first_tag)
+        backpointer.append(first_backpointer)
 
-        tags = []
-        back_path = best_prob["{}{}".format(i+1, " </s>")]
-        while back_path != "0 <s>":
-            tag = back_path.split(" ")[1]
-            tags.append(tag)
-            back_path = best_prob[back_path]
-        tags.reverse()
-        for i in range(0, length-1):
-            out_file.write(u"{}/{} ".format(words[i], tags[i]))
-        out_file.write(u"{}/{}\n".format(words[length-1], tags[length-1]))
-        line = None
+        for i in range(1, len(words)):
+            cur_word = words[i]
+            this_tag = {}
+            this_backpointer = {}
+            prev_tags = viterbi[-1]
+            if cur_word in emission:
+                possible_tags = emission[cur_word].keys()
+            else:
+                possible_tags = tag_set
+            for tag in possible_tags:
+                if tag == "<s>":
+                    continue
+                best_previous = prev_tags.keys()[0]
+                max = 0.0
+                for prevtag in prev_tags.keys():
+                    prob = prev_tags[prevtag] * transition_probability(prevtag, tag,t_len) * emission_probability(words[i],tag)
+                    if prob >= max:
+                        max = prob
+                        best_previous = prevtag
+                this_tag[tag] = prev_tags[best_previous] * transition_probability(best_previous,tag,t_len) * emission_probability(words[i],tag)
+                this_backpointer[tag] = best_previous
+            max = 0.0
+            currbest = this_tag.keys()[0]
+            for tag in this_tag.keys():
+                if this_tag[tag] >= max:
+                    max = this_tag[tag]
+                    currbest = tag
+
+            viterbi.append(this_tag)
+            backpointer.append(this_backpointer)
+        prev_tags = viterbi[-1]
+        best_previous = prev_tags.keys()[0]
+        max = 0.0
+        for prevtag in prev_tags.keys():
+            prob = prev_tags[prevtag] * transition_probability(prevtag,currbest,t_len)
+            if prob >= max:
+                max = prob
+                best_previous = prevtag
+
+        tag_sequence = [best_previous]
+        backpointer.reverse()
+        cur_tag = best_previous
+        for bp in backpointer:
+            tag_sequence.append(bp[cur_tag])
+            cur_tag = bp[cur_tag]
+        tag_sequence.pop()
+        tag_sequence.reverse()
+        write_to_file(tag_sequence, line)
+        line = input_file.readline()
+
+
+def write_to_file(tag_sequence, line):
+    tagged_words = []
+    words = line.split()
+    for i in range(0, len(words)):
+        tagged_words.append(u"{}/{}".format(words[i], tag_sequence[i]))
+    out_file.write(' '.join(tagged_words))
+    out_file.write("\n")
+
+
+def transition_probability(prev, nxt, t_len):
+    if prev in transition and nxt in transition[prev]:
+        prob = float(transition[prev][nxt] + 1) / (float(tag_count[prev]) + t_len)
+    else:
+        prob = float(1)/(float(tag_count[prev] + t_len))
+    return prob
+
+
+def emission_probability(word, tag):
+    if word in emission and tag in emission[word]:
+        return float(emission[word][tag])/ tag_count[tag]
+    else:
+        return 1
 
 
 def main():
+    s = time.time()
     read_model()
     path = sys.argv[1]
     process_input(path)
     out_file.close()
     model.close()
+    e = time.time()
+    print(e-s)
 main()
 
--- hmmlearn.py
+++ hmmlearn.py
@@ -2,10 +2,11 @@ import sys
 import codecs
 import time
 
-model = codecs.open("hmmmodel.txt", "w",'utf-8')
-emission = dict()
-transitions = dict()
+model = codecs.open("hmmmodel.txt", "w", 'utf-8')
+emission = {}
+transitions = {}
 count = dict()
+last_tags = dict()
 vocab = set()
 
 
@@ -13,67 +14,54 @@ def read_data(train_file):
     data_file = codecs.open(train_file, "r", "utf-8")
     train_data = data_file.readlines()
     for line in train_data:
-        word_tag_list = line.strip().split(" ")
         previous = "<s>"
-        if previous in count:
-            count[previous] += 1
-        else:
-            count[previous] = 1
-        for word_tag in word_tag_list:
+        for word_tag in line.split():
             word = word_tag[:-3]
-            vocab.add(word)
             tag = word_tag[-2:]
-            if previous + " " + tag in transitions:
-                transitions[previous + " " + tag] += 1
+            vocab.add(word)
+            # Construct a transition matrix
+            if previous in transitions:
+                if tag in transitions[previous]:
+                    transitions[previous][tag] += 1
+                else:
+                    transitions[previous][tag] = 1
+                count[previous] += 1
             else:
-                transitions[previous + " " + tag] = 1
-            if tag in count:
-                count[tag] += 1
+                transitions[previous] = {}
+                transitions[previous][tag] = 1
+                count[previous] = 1
+            # Construct Emission matrix
+            if word in emission:
+                if tag in emission[word]:
+                    emission[word][tag] += 1
+                else:
+                    emission[word][tag] = 1
             else:
-                count[tag] = 1
-            if tag+' '+word in emission:
-                emission[tag+" "+word] += 1
+                emission[word] = {}
+                emission[word][tag] = 1
+            if tag in last_tags:
+                last_tags[tag] += 1
             else:
-                emission[tag+" "+word] = 1
+                last_tags[tag] = 1
             previous = tag
-        if previous + " </s>" not in transitions:
-            transitions[previous + " </s>"] = 1
-        else:
-            transitions[previous + " </s>"] += 1
-            # word_tag_file.write(u'{} - {}\n'.format(word, tag).encode('utf-8'))
 
 
 def generate_model():
-    v_len = len(vocab)
-    t_len = len(count)
     for key, value in transitions.iteritems():
-        previous = key.split(" ")[0]
-        probability = (float(value)+1)/(count[previous]+ t_len)
-        model.write(u'T {} {}\n'.format(key, probability))
+        model.write(u"T {}\n{}\n".format(key, value))
     for key, value in emission.iteritems():
-        tag = key.split(" ")[0]
-        probability = float(value + 1)/(count[tag] + v_len)
-        model.write(u'E {} {}\n'.format(key, probability))
-
-
-def normalize():
-    l = len(count)
-    for word in count.keys():
-        if word+" </s>" not in transitions:
-            transitions[word+" </s>"] = 1 / l
-    for u in count.keys():
-        for v in count.keys():
-            if u+" "+v not in transitions:
-                transitions[u+" "+v] = 1 / l
+        model.write(u"E {}\n{}\n".format(key, value))
+    model.write(u"Count\n{}\n".format(count))
+    model.write(u"Last Tags\n{}\n".format(last_tags))
+    model.close()
 
 
 def main():
     s = time.time()
     train_file = sys.argv[1]
     read_data(train_file)
-    normalize()
+    # print time.time()-s
     generate_model()
-    model.close()
     end = time.time()
     print end-s
 main()
